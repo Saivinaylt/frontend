016e54c2cb695b19f4830c5afbc2f598d5cae2af
Saivinay <saivinay165@pec.edu>
2023-01-10 14:22:03 +0530

    front end commit change

---
+++ catalogue.sh
@@ -0,0 +1,26 @@
+location_file=$(pwd)
+
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+cp ${location_file}/files/catalogue.service /etc/systemd/system/catalogue.service
+
+systemctl daemon-reload
+
+systemctl enable catalogue
+systemctl start catalogue
+
+#mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js

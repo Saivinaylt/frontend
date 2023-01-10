8a0021b711cffa388a5a718cbe2c301b84d81410
Saivinay <saivinay165@pec.edu>
2023-01-10 10:27:31 +0530

    front end commit change

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,10 @@
 location_file=$(pwd)
+cp ${location_file}/files/mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
 
-cp ${location_file}/files/mongo.repo /etc/yum.repos.d/mongo.repo
+
 
 systemctl enable mongod
 systemctl start mongod
 
-systemctl restart mongod

c97b106548eac69e0973379d5289fa3a3bc78457
Saivinay <saivinay165@pec.edu>
2023-01-11 18:50:28 +0530

    front end commit change set -e

--- frontend.sh
+++ frontend.sh
@@ -5,27 +5,27 @@ echo -e "\e[35m install nginx\e[0m"
 yum install nginx -y &>>${log}
 
 echo -e "\e[36m enable nginx\e[0m"
-systemctl enable nginx
+systemctl enable nginx &>>${log}
 
 echo -e "\e[36m start nginx\e[0m"
-systemctl start nginx
+systemctl start nginx &>>${log}
 
 echo -e "\e[36m removing file\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${log}
 
 echo -e "\e[36m download\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 
 echo -e "\e[36m directory changing\e[0m"
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>${log}
 
 echo -e "\e[36m unzip nginx\e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${log}
 
 echo -e "\e[36m conf location change\e[0m"
 
-cp ${location_file}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${location_file}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
 
 echo -e "\e[36m restart nginx\e[0m"
-systemctl restart nginx
+systemctl restart nginx &>>${log}
 

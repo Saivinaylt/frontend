baecb6ec86eee933af163059ba1d282dcdc66ed4
Saivinay <saivinay165@pec.edu>
2023-01-11 18:53:26 +0530

    front end commit change set -e

--- frontend.sh
+++ frontend.sh
@@ -7,26 +7,32 @@ echo $?
 
 echo -e "\e[36m enable nginx\e[0m"
 systemctl enable nginx &>>${log}
+echo $?
 
 echo -e "\e[36m start nginx\e[0m"
 systemctl start nginx &>>${log}
+echo $?
 
 echo -e "\e[36m removing file\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${log}
+echo $?
 
 echo -e "\e[36m download\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
+echo $?
 
 echo -e "\e[36m directory changing\e[0m"
 cd /usr/share/nginx/html &>>${log}
+echo $?
 
 echo -e "\e[36m unzip nginx\e[0m"
 unzip /tmp/frontend.zip &>>${log}
+echo $?
 
 echo -e "\e[36m conf location change\e[0m"
-
 cp ${location_file}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
+echo $?
 
 echo -e "\e[36m restart nginx\e[0m"
 systemctl restart nginx &>>${log}
-
+echo $?

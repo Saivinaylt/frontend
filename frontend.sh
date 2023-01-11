79f0ee85cd706f9aca5da84faf7a86ba4b97409c
Saivinay <saivinay165@pec.edu>
2023-01-11 19:05:49 +0530

    front end commit change set -e

--- frontend.sh
+++ frontend.sh
@@ -3,11 +3,21 @@ log=/tmp/robosho.log
 
 echo -e "\e[35m install nginx\e[0m"
 yum install nginx -y &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo install nginx sucessfully
+  else
+    echo unsucessfull
+  fi
 
 echo -e "\e[36m enable nginx\e[0m"
 systemctl enable nginx &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo enable nginx sucessfully
+  else
+    echo unsucessfull
+  fi
 
 echo -e "\e[36m start nginx\e[0m"
 systemctl start nginx &>>${log}
@@ -21,24 +31,54 @@ then
 
 echo -e "\e[36m removing file\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo removing file sucessfully
+  else
+    echo unsucessfull
+  fi
 
 echo -e "\e[36m download\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo download sucessfully
+  else
+    echo unsucessfull
+  fi
 
 echo -e "\e[36m directory changing\e[0m"
 cd /usr/share/nginx/html &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo directory changing sucessfully
+  else
+    echo unsucessfull
+  fi
 
 echo -e "\e[36m unzip nginx\e[0m"
 unzip /tmp/frontend.zip &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo unzip nginx sucessfully
+  else
+    echo unsucessfull
+  fi
 
 echo -e "\e[36m conf location change\e[0m"
 cp ${location_file}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo conf location change sucessfully
+  else
+    echo unsucessfull
+  fi
 
 echo -e "\e[36m restart nginx\e[0m"
 systemctl restart nginx &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo restart nginx sucessfully
+  else
+    echo unsucessfull
+  fi
